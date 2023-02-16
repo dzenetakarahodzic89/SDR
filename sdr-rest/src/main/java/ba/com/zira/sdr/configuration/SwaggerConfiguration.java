721c64851bada7619b5ad2120803d8afd0bec09a
Armin Petrović <apetrovic1@etf.unsa.ba>
2023-02-17 00:07:23 +0100

    Added route API to swagger UI

--- sdr-rest/src/main/java/ba/com/zira/sdr/configuration/SwaggerConfiguration.java
+++ sdr-rest/src/main/java/ba/com/zira/sdr/configuration/SwaggerConfiguration.java
@@ -33,6 +33,12 @@ public class SwaggerConfiguration {
     }
 
     @Bean
+    public GroupedOpenApi artistSongApi() {
+        return GroupedOpenApi.builder().group("songartist-api").packagesToScan("ba.com.zira.sdr.songartist.rest")
+                .addOperationCustomizer(ziraOperationCustomizer).build();
+    }
+
+    @Bean
     public OpenAPI ziraOpenAPI() {
         return new OpenAPI().info(new Info().title("ZIRA API").description("ZIRA API").version("0.0.1-SNAPSHOT")
                 .contact(new Contact().name("ZIRA").url("http://www.zira.com.ba").email("info@zira.com.ba"))
