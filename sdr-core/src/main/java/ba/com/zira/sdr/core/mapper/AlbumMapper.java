9c1766880680103b75bdbb589bfe17e4c2b9b355
Velid Imiroviæ <vimsirovic1@etf.unsa.ba>
2023-02-19 23:56:36 +0100

    Created AlbumMapper and Validator

--- sdr-core/src/main/java/ba/com/zira/sdr/core/mapper/AlbumMapper.java
+++ sdr-core/src/main/java/ba/com/zira/sdr/core/mapper/AlbumMapper.java
@@ -1,9 +1,14 @@
 package ba.com.zira.sdr.core.mapper;
 
+import java.util.List;
+
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
 
 import ba.com.zira.sdr.api.model.album.AlbumCreateRequest;
+import ba.com.zira.sdr.api.model.album.AlbumResponse;
+import ba.com.zira.sdr.api.model.album.AlbumUpdateRequest;
 import ba.com.zira.sdr.dao.model.AlbumEntity;
 
 @Mapper(componentModel = "spring")
@@ -13,6 +18,10 @@ public interface AlbumMapper {
     AlbumEntity dtoToEntity(AlbumCreateRequest albumCreateRequest);
 
     @Mapping(source = "era.id", target = "eraId")
-    AlbumCreateRequest entityToDto(AlbumEntity albumEntity);
+    AlbumResponse entityToDto(AlbumEntity albumEntity);
 
+    @Mapping(source = "eraId", target = "era.id")
+    void updateEntity(AlbumUpdateRequest albumModel, @MappingTarget AlbumEntity albumEntity);
+
+    List<AlbumResponse> entitiesToDtos(List<AlbumEntity> albumEntities);
 }
