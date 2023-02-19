f8e729cb275b7cd3eb61db3b8cfd55cc11500d53
Velid Imiroviæ <vimsirovic1@etf.unsa.ba>
2023-02-19 23:55:58 +0100

    Created AlbumService Interface

--- sdr-api/src/main/java/ba/com/zira/sdr/api/AlbumService.java
+++ sdr-api/src/main/java/ba/com/zira/sdr/api/AlbumService.java
@@ -5,16 +5,18 @@ import ba.com.zira.commons.message.request.EntityRequest;
 import ba.com.zira.commons.message.request.FilterRequest;
 import ba.com.zira.commons.message.response.PagedPayloadResponse;
 import ba.com.zira.commons.message.response.PayloadResponse;
-import ba.com.zira.sdr.api.model.SampleModel;
-import ba.com.zira.sdr.api.model.SampleModelCreateRequest;
-import ba.com.zira.sdr.api.model.SampleModelUpdateRequest;
+import ba.com.zira.sdr.api.model.album.AlbumCreateRequest;
+import ba.com.zira.sdr.api.model.album.AlbumResponse;
+import ba.com.zira.sdr.api.model.album.AlbumUpdateRequest;
 
 public interface AlbumService {
-    public PagedPayloadResponse<SampleModel> find(final FilterRequest request) throws ApiException;
+    public PagedPayloadResponse<AlbumResponse> find(final FilterRequest request) throws ApiException;
 
-    PayloadResponse<SampleModel> create(EntityRequest<SampleModelCreateRequest> request) throws ApiException;
+    PayloadResponse<AlbumResponse> create(EntityRequest<AlbumCreateRequest> request) throws ApiException;
 
-    PayloadResponse<SampleModel> update(EntityRequest<SampleModelUpdateRequest> request) throws ApiException;
+    PayloadResponse<AlbumResponse> update(EntityRequest<AlbumUpdateRequest> request) throws ApiException;
 
-    PayloadResponse<SampleModel> activate(EntityRequest<Long> request) throws ApiException;
+    PayloadResponse<AlbumResponse> delete(EntityRequest<Long> request) throws ApiException;
+
+    PayloadResponse<AlbumResponse> activate(EntityRequest<Long> request) throws ApiException;
 }
