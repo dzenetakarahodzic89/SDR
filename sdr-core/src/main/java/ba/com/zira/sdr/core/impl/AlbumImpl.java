bfa2b8b231a079d253acd50bebe6aaada403d518
Velid Imiroviæ <vimsirovic1@etf.unsa.ba>
2023-02-19 23:56:13 +0100

    Created AlbumImpl

--- sdr-core/src/main/java/ba/com/zira/sdr/core/impl/AlbumImpl.java
+++ sdr-core/src/main/java/ba/com/zira/sdr/core/impl/AlbumImpl.java
@@ -1,5 +1,82 @@
 package ba.com.zira.sdr.core.impl;
 
-public class AlbumImpl {
+import java.time.LocalDateTime;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import ba.com.zira.commons.exception.ApiException;
+import ba.com.zira.commons.message.request.EntityRequest;
+import ba.com.zira.commons.message.request.FilterRequest;
+import ba.com.zira.commons.message.response.PagedPayloadResponse;
+import ba.com.zira.commons.message.response.PayloadResponse;
+import ba.com.zira.commons.model.PagedData;
+import ba.com.zira.commons.model.enums.Status;
+import ba.com.zira.commons.model.response.ResponseCode;
+import ba.com.zira.sdr.api.AlbumService;
+import ba.com.zira.sdr.api.model.album.AlbumCreateRequest;
+import ba.com.zira.sdr.api.model.album.AlbumResponse;
+import ba.com.zira.sdr.api.model.album.AlbumUpdateRequest;
+import ba.com.zira.sdr.core.mapper.AlbumMapper;
+import ba.com.zira.sdr.core.validation.AlbumRequestValidation;
+import ba.com.zira.sdr.dao.AlbumDAO;
+import ba.com.zira.sdr.dao.model.AlbumEntity;
+import lombok.AllArgsConstructor;
+
+@Service
+@AllArgsConstructor
+public class AlbumImpl implements AlbumService {
+
+    AlbumDAO albumDAO;
+    AlbumMapper albumMapper;
+    AlbumRequestValidation albumRequestValidation;
+
+    @Override
+    public PagedPayloadResponse<AlbumResponse> find(final FilterRequest request) throws ApiException {
+        PagedData<AlbumEntity> albumEntities = albumDAO.findAll(request.getFilter());
+        return new PagedPayloadResponse<>(request, ResponseCode.OK, albumEntities, albumMapper::entitiesToDtos);
+    }
+
+    @Override
+    @Transactional(rollbackFor = Exception.class)
+    public PayloadResponse<AlbumResponse> create(final EntityRequest<AlbumCreateRequest> request) throws ApiException {
+        albumRequestValidation.validateCreateAlbumRequest(request);
+        var albumEntity = albumMapper.dtoToEntity(request.getEntity());
+        albumEntity.setCreated(LocalDateTime.now());
+        albumEntity.setStatus(Status.ACTIVE.value());
+        albumEntity.setCreatedBy(request.getUserId());
+        albumEntity.setModified(LocalDateTime.now());
+        albumEntity.setModifiedBy(request.getUserId());
+        albumDAO.persist(albumEntity);
+        return new PayloadResponse<>(request, ResponseCode.OK, albumMapper.entityToDto(albumEntity));
+    }
+
+    @Override
+    @Transactional(rollbackFor = Exception.class)
+    public PayloadResponse<AlbumResponse> update(final EntityRequest<AlbumUpdateRequest> request) throws ApiException {
+        albumRequestValidation.validateUpdateAlbumRequest(request);
+        var albumEntity = albumDAO.findByPK(request.getEntity().getId());
+        albumMapper.updateEntity(request.getEntity(), albumEntity);
+        albumEntity.setModifiedBy(request.getUserId());
+        albumEntity.setModified(LocalDateTime.now());
+
+        albumDAO.merge(albumEntity);
+        return new PayloadResponse<>(request, ResponseCode.OK, albumMapper.entityToDto(albumEntity));
+    }
+
+    @Override
+    @Transactional(rollbackFor = Exception.class)
+    public PayloadResponse<AlbumResponse> delete(final EntityRequest<Long> request) throws ApiException {
+        albumRequestValidation.validateDeleteAlbumRequest(request);
+        var albumEntity = albumDAO.findByPK(request.getEntity());
+        albumDAO.removeByPK(request.getEntity());
+        return new PayloadResponse<>(request, ResponseCode.OK, albumMapper.entityToDto(albumEntity));
+    }
+
+    @Override
+    public PayloadResponse<AlbumResponse> activate(final EntityRequest<Long> request) throws ApiException {
+        // TODO Auto-generated method stub
+        return null;
+    }
 
 }
