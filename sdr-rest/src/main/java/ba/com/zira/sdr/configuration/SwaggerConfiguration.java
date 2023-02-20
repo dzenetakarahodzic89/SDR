7c90e63cbda79045b2acef28b47133f5ad7c0190
Dado <Dado.Bajramovic@zira.com.ba>
2023-02-20 16:39:12 +0100

    merge fix

--- sdr-rest/src/main/java/ba/com/zira/sdr/configuration/SwaggerConfiguration.java
+++ sdr-rest/src/main/java/ba/com/zira/sdr/configuration/SwaggerConfiguration.java
@@ -13,53 +13,40 @@ import io.swagger.v3.oas.models.info.License;
 @Configuration
 public class SwaggerConfiguration {
 
-	private final ZiraOperationCustomizer ziraOperationCustomizer;
+    private final ZiraOperationCustomizer ziraOperationCustomizer;
 
-	public SwaggerConfiguration(final ZiraOperationCustomizer ziraOperationCustomizer) {
-		super();
-		this.ziraOperationCustomizer = ziraOperationCustomizer;
-	}
+    public SwaggerConfiguration(final ZiraOperationCustomizer ziraOperationCustomizer) {
+        super();
+        this.ziraOperationCustomizer = ziraOperationCustomizer;
+    }
 
-	@Bean
-	public GroupedOpenApi personApi() {
-		return GroupedOpenApi.builder().group("person-api").packagesToScan("ba.com.zira.sdr.person.rest")
-				.addOperationCustomizer(ziraOperationCustomizer).build();
-	}
+    @Bean
+    public GroupedOpenApi personApi() {
+        return GroupedOpenApi.builder().group("person-api").packagesToScan("ba.com.zira.sdr.person.rest")
+                .addOperationCustomizer(ziraOperationCustomizer).build();
+    }
 
-	@Bean
-	public GroupedOpenApi genreApi() {
-		return GroupedOpenApi.builder().group("genre-api").packagesToScan("ba.com.zira.sdr.genre.rest")
-				.addOperationCustomizer(ziraOperationCustomizer).build();
-	}
+    @Bean
+    public GroupedOpenApi genreApi() {
+        return GroupedOpenApi.builder().group("genre-api").packagesToScan("ba.com.zira.sdr.genre.rest")
+                .addOperationCustomizer(ziraOperationCustomizer).build();
+    }
 
-	@Bean
-	public GroupedOpenApi multisearchApi() {
-		return GroupedOpenApi.builder().group("multisearch-api").packagesToScan("ba.com.zira.sdr.multisearch.rest")
-				.addOperationCustomizer(ziraOperationCustomizer).build();
-	}
-
-	@Bean
-	public GroupedOpenApi artistSongApi() {
-		return GroupedOpenApi.builder().group("songartist-api").packagesToScan("ba.com.zira.sdr.songartist.rest")
-				.addOperationCustomizer(ziraOperationCustomizer).build();
-	}
-
-	@Bean
-	public GroupedOpenApi sampleApi() {
-		return GroupedOpenApi.builder().group("sample-api").packagesToScan("ba.com.zira.sdr.rest")
-				.addOperationCustomizer(ziraOperationCustomizer).build();
-	}
-
-	@Bean
-	public OpenAPI ziraOpenAPI() {
-		return new OpenAPI().info(new Info().title("ZIRA API").description("ZIRA API").version("0.0.1-SNAPSHOT")
-				.contact(new Contact().name("ZIRA").url("http://www.zira.com.ba").email("info@zira.com.ba"))
-				.license(new License().name("ZIRA proprietary service").url("http://www.zira.com.ba/")));
-	}
+    @Bean
+    public GroupedOpenApi multisearchApi() {
+        return GroupedOpenApi.builder().group("multisearch-api").packagesToScan("ba.com.zira.sdr.multisearch.rest")
+                .addOperationCustomizer(ziraOperationCustomizer).build();
+    }
 
     @Bean
     public GroupedOpenApi artistSongApi() {
         return GroupedOpenApi.builder().group("songartist-api").packagesToScan("ba.com.zira.sdr.songartist.rest")
+                .addOperationCustomizer(ziraOperationCustomizer).build();
+    }
+
+    @Bean
+    public GroupedOpenApi sampleApi() {
+        return GroupedOpenApi.builder().group("sample-api").packagesToScan("ba.com.zira.sdr.rest")
                 .addOperationCustomizer(ziraOperationCustomizer).build();
     }
 
@@ -75,7 +62,5 @@ public class SwaggerConfiguration {
                 .contact(new Contact().name("ZIRA").url("http://www.zira.com.ba").email("info@zira.com.ba"))
                 .license(new License().name("ZIRA proprietary service").url("http://www.zira.com.ba/")));
     }
-    
-    
 
 }
