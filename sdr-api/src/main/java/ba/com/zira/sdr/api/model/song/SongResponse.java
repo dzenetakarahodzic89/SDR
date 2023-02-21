858a032b277327fe83a356169a8be39a5d1ee9f7
Velid Imiroviæ <vimsirovic1@etf.unsa.ba>
2023-02-21 13:40:35 +0100

    Removed atributes from SongResponse

--- sdr-api/src/main/java/ba/com/zira/sdr/api/model/song/SongResponse.java
+++ sdr-api/src/main/java/ba/com/zira/sdr/api/model/song/SongResponse.java
@@ -1,7 +1,6 @@
 package ba.com.zira.sdr.api.model.song;
 
 import java.io.Serializable;
-import java.time.LocalDateTime;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
@@ -20,39 +19,11 @@ public class SongResponse implements Serializable {
     private Long id;
 
     @NotBlank
-    @Schema(description = "Created")
-    private LocalDateTime created;
-
-    @NotBlank
-    @Schema(description = "Created by")
-    private String createdBy;
-
-    @NotBlank
-    @Schema(description = "Date of release")
-    private LocalDateTime dateOfRelease;
-
-    @NotBlank
-    @Schema(description = "Information")
-    private String information;
-
-    @NotBlank
-    @Schema(description = "Modified")
-    private LocalDateTime modified;
-
-    @NotBlank
-    @Schema(description = "Modified by")
-    private String modifiedBy;
-
-    @NotBlank
     @Schema(description = "Name")
     private String name;
 
     @NotBlank
     @Schema(description = "Playtime")
     private String playtime;
-
-    @NotBlank
-    @Schema(description = "Status")
-    private String status;
 
 }
