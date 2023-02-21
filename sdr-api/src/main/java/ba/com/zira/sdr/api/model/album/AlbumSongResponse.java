7335ef1d69552b42412702c6e04e0caa061d322f
Velid Imiroviæ <vimsirovic1@etf.unsa.ba>
2023-02-21 13:05:21 +0100

    Created AlbumSongResponse

---
+++ sdr-api/src/main/java/ba/com/zira/sdr/api/model/album/AlbumSongResponse.java
@@ -0,0 +1,24 @@
+package ba.com.zira.sdr.api.model.album;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.validation.constraints.NotBlank;
+
+import ba.com.zira.sdr.api.model.song.SongResponse;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Data;
+
+@Data
+@Schema(description = "Properties of response for route album/{id}/songs")
+public class AlbumSongResponse implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @NotBlank
+    @Schema(description = "Release date of album")
+    List<SongResponse> songs;
+
+    @NotBlank
+    @Schema(description = "Total playtime")
+    String totalPlayTime;
+}
