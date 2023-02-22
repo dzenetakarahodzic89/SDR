96c07566e1741ee6b9fce74c62e3c67a5123b5a8
Mahir Prcanović <mahirprcanovic7@gmail.com>
2023-02-22 20:18:25 +0100

    feat: test created, service test done

---
+++ sdr-core/src/test/java/ba/com/zira/sdr/test/suites/ChordProgressionRequestValidationTest.java
@@ -0,0 +1,7 @@
+package ba.com.zira.sdr.test.suites;
+
+import ba.com.zira.sdr.test.configuration.BasicTestConfiguration;
+
+public class ChordProgressionRequestValidationTest extends BasicTestConfiguration {
+
+}
