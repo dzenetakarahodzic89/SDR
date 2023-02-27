56df8111b691a372f92daf0f5560e4848dd55621
Dado <Dado.Bajramovic@zira.com.ba>
2023-02-27 16:04:24 +0100

    Annotation fix

--- sdr-dao/src/main/java/ba/com/zira/sdr/dao/SongSimilarityDAO.java
+++ sdr-dao/src/main/java/ba/com/zira/sdr/dao/SongSimilarityDAO.java
@@ -1,7 +1,10 @@
 package ba.com.zira.sdr.dao;
 
+import org.springframework.stereotype.Repository;
+
 import ba.com.zira.commons.dao.AbstractDAO;
 import ba.com.zira.sdr.dao.model.SongSimilarityDetailEntity;
 
+@Repository
 public class SongSimilarityDAO extends AbstractDAO<SongSimilarityDetailEntity, Long> {
 }
