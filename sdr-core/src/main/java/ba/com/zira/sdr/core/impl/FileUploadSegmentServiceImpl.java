5dd0fb74f85105a93b754b86b4e495ae99f03a7d
Mahir Prcanović <mahirprcanovic7@gmail.com>
2023-03-04 02:02:49 +0100

    feat: upload audio backend done, needs edge cases

--- sdr-core/src/main/java/ba/com/zira/sdr/core/impl/FileUploadSegmentServiceImpl.java
+++ sdr-core/src/main/java/ba/com/zira/sdr/core/impl/FileUploadSegmentServiceImpl.java
@@ -8,9 +8,9 @@ import org.springframework.stereotype.Service;
 import ba.com.zira.commons.exception.ApiException;
 import ba.com.zira.commons.message.request.EntityRequest;
 import ba.com.zira.commons.message.response.PayloadResponse;
-import ba.com.zira.commons.model.enums.Status;
 import ba.com.zira.commons.model.response.ResponseCode;
 import ba.com.zira.sdr.api.FileUploadSegmentService;
+import ba.com.zira.sdr.api.enums.FileUploadSegmentStatus;
 import ba.com.zira.sdr.api.model.fileuploadsegment.FileUploadSegmentCreateRequest;
 import ba.com.zira.sdr.core.mapper.FileUploadSegmentMapper;
 import ba.com.zira.sdr.dao.FileUploadSegmentDAO;
@@ -41,8 +41,11 @@ public class FileUploadSegmentServiceImpl implements FileUploadSegmentService {
         saveFileSegmentEntity.setId(UUID.randomUUID().toString());
         saveFileSegmentEntity.setCreated(LocalDateTime.now());
         saveFileSegmentEntity.setCreatedBy(request.getUserId());
-        saveFileSegmentEntity.setStatus(Status.ACTIVE.value());
+        saveFileSegmentEntity.setStatus(FileUploadSegmentStatus.UPLOADING.getValue());
         fileUploadSegmentDAO.persist(saveFileSegmentEntity);
+        if (saveFileSegmentEntity.getFileSegment().equals(saveFileSegmentEntity.getFileSegmentTotal())) {
+            fileUploadSegmentDAO.updateStatus(FileUploadSegmentStatus.READY_TO_MERGE.getValue(), saveFileSegmentEntity.getMedia().getId());
+        }
         return new PayloadResponse<>(request, ResponseCode.OK, "ok");
     }
 
--- sdr-dao/src/main/java/ba/com/zira/sdr/dao/FileUploadSegmentDAO.java
+++ sdr-dao/src/main/java/ba/com/zira/sdr/dao/FileUploadSegmentDAO.java
@@ -1,5 +1,7 @@
 package ba.com.zira.sdr.dao;
 
+import javax.persistence.Query;
+
 import org.springframework.stereotype.Repository;
 
 import ba.com.zira.commons.dao.AbstractDAO;
@@ -7,5 +9,9 @@ import ba.com.zira.sdr.dao.model.FileUploadSegmentEntity;
 
 @Repository
 public class FileUploadSegmentDAO extends AbstractDAO<FileUploadSegmentEntity, String> {
-
+    public void updateStatus(String status, Long mediaId) {
+        var hql = "update FileUploadSegmentEntity set status = :status where media.id = :mediaId";
+        Query query = entityManager.createQuery(hql).setParameter("status", status).setParameter("mediaId", mediaId);
+        query.executeUpdate();
+    }
 }
