91f61ddc5d899fc149e0676c6ab6a014eb184052
Mahir Prcanović <mahirprcanovic7@gmail.com>
2023-03-04 01:22:49 +0100

    feat: dao implementation

---
+++ sdr-dao/src/main/java/ba/com/zira/sdr/dao/FileUploadSegmentDAO.java
@@ -0,0 +1,11 @@
+package ba.com.zira.sdr.dao;
+
+import org.springframework.stereotype.Repository;
+
+import ba.com.zira.commons.dao.AbstractDAO;
+import ba.com.zira.sdr.dao.model.FileUploadSegmentEntity;
+
+@Repository
+public class FileUploadSegmentDAO extends AbstractDAO<FileUploadSegmentEntity, String> {
+
+}
